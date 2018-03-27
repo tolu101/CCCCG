b1abb506a7d5728b862b0f2861928825d269ba40
tolu101 <toluaroyehun@yahoo.co.uk>
2018-03-27 12:53:44 +0100

    added login in code about to test

--- app/src/main/java/com/example/user/cg/login.java
+++ app/src/main/java/com/example/user/cg/login.java
@@ -1,17 +1,96 @@
 package com.example.user.cg;
 
+import android.app.ProgressDialog;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.user.cg.Model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class login extends AppCompatActivity {
 
 
-    Button SignInbtn,
+    Button SignInbtn;
+    TextView txtNoAccount;
+    EditText edtPhone, edtPassword;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        edtPassword = (MaterialEditText)findViewById(R.id.edtPassword);
+        edtPhone = (MaterialEditText)findViewById(R.id.edtPhone);
+        SignInbtn = (Button)findViewById(R.id.SignInbtn);
+
+
+
+
+       //initial firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_user = database.getReference("User");
+
+        SignInbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final ProgressDialog mDialog = new ProgressDialog(login.this);
+                mDialog.setMessage("Please waiting....");
+                mDialog.show();
+
+
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        //check if user not exist in database
+                        if(dataSnapshot.child(edtPhone.getText().toString()).exists()) {
+
+
+                            //Get user information
+                            mDialog.dismiss();
+                            User user = dataSnapshot.child(edtPhone.getText().toString()).getValue(User.class);
+                            if (user.getPassword().equals(edtPassword.getText().toString())) {
+                                Toast.makeText(login.this, "Sign in successfully!", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(login.this, "Sign in failed!", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                        else
+                        {
+                            Toast.makeText(login.this, "user not exist in Database", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+        //set font for the text for register
+        txtNoAccount = (TextView)findViewById(R.id.txtNoAccount);
+        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/Lato-Hairline.ttf");
+        txtNoAccount.setTypeface(face);
+
+
+
+
+
     }
 }
